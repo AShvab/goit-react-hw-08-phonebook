5ef85843bd323a601dc7f8b39f0b1e40f80ba9cb
AShvab <shvabovska@gmail.com>
2023-06-28 22:25:34 +0300

    add useEffect

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,9 +1,26 @@
+// import { Helmet } from 'react-helmet';
+// import LoginForm  from 'components/LoginForm/LoginForm';
+
+// export default function Login() {
+//   return (
+//     <div>
+//       <Helmet>
+//         <title>Login</title>
+//       </Helmet>
+//       <LoginForm />
+//     </div>
+//   );
+// }
+import React, { useEffect } from 'react';
 import LoginForm from 'components/LoginForm/LoginForm';
 
 export default function Login() {
+  useEffect(() => {
+    document.title = 'Login';
+  }, []);
+
   return (
     <div>
-      <title>Login</title>
       <LoginForm />
     </div>
   );
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,10 +1,27 @@
+// import RegisterForm from 'components/RegisterForm/RegisterForm';
+// import { Helmet } from 'react-helmet';
+
+// export default function Register() {
+//   return (
+//     <div>
+//       <Helmet>
+//         <title>Registration</title>
+//       </Helmet>
+//       <RegisterForm /> 
+//     </div>
+//   );
+// }
+import React, { useEffect } from 'react';
 import RegisterForm from 'components/RegisterForm/RegisterForm';
 
 export default function Register() {
+  useEffect(() => {
+    document.title = 'Registration';
+  }, []);
+
   return (
     <div>
-        <title>Registration</title>
-      <RegisterForm /> 
+      <RegisterForm />
     </div>
   );
 }
